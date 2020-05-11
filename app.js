4d1b37a97ae09c62acfcb8ed3a46ad636f52a7d0
manshowsir <jamjikung@icloud.com>
2020-05-12 01:23:39 +0800

    edit

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 App({
   onLaunch: function () {
-    
+    let that = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        console.log('env', res);
+        if (res.platform == "devtools")
+          that.globalData.url.host = that.globalData.url.host_dev;
+        else
+          that.globalData.url.host = that.globalData.url.host_real;
+      },
+    })
   },
-  globalData: {}
+  globalData: {
+    url: {
+      host: "",
+      host_dev: "https://wxapphis.gzfuture.cn",
+      host_real: "https://wxapphis.gzfuture.cn"
+    },
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,6 @@
-const app = getApp()
+const app = getApp();
+const cRequest = require('../../comFunc/cRequest.js');
+const createRecycleContext = require('miniprogram-recycle-view');
 
 Page({
   data: {
@@ -7,5 +9,46 @@ Page({
   
   onLoad: function () {
     
+  },
+
+  onReady: function() {
+    let that = this
+    wx.showLoading({
+      title: '数据获取中',
+    })
+    cRequest.requstGet({
+      appName: 'IMall',
+      funcName: 'GetCusList',
+      data: {
+        id:'r41b5454764',
+        type: 'nav'
+      }
+    }).then(res => {
+      console.log(JSON.parse(res))
+      wx.setNavigationBarTitle({
+        title: JSON.parse(res).title
+      })
+      var ctx = createRecycleContext({
+        id: 'recycleId',
+        dataKey: 'recycleList',
+        page: that,
+        itemSize: { // 这个参数也可以直接传下面定义的this.itemSizeFunc函数
+          width: 162,
+          height: 182
+        }
+      })
+      let listAppend = JSON.parse(res).list
+      ctx.append(listAppend)
+      // ctx.update(beginIndex, list)
+      // ctx.destroy()
+    });
+    wx.hideLoading();
+    
+  },
+  itemSizeFunc: function (item, idx) {
+    return {
+        width: 162,
+        height: 182
+    }
   }
 })
